6577cd25fb58322fc35fcc0d1c130721c1784b2e
StellaSK <stellakotacho@hotmail.com>
2022-09-18 15:16:18 -0300

    Criação do Repositorio.

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+        System.out.println("Total soma: " + resultado);
+    }
+
+    public static void subtracao(double numero1, double numero2) {
+        double resultado = numero1 - numero2;
+        System.out.println("Total subtracao: " + resultado);
+    }
+
+    public static void multiplicacao(double numero1, double numero2) {
+        double resultado = numero1 * numero2;
+        System.out.println("Total multiplicacao: " + resultado);
+    }
+
+    public static void divisao(double numero1, double numero2) {
+        double resultado = numero1 / numero2;
+        System.out.println("Total divisao: " + resultado);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        double numero1, numero2;
+        double totalSoma;
+
+        // CALCULADORA
+        System.out.println("CALCULADORA");
+        System.out.println("=====================================");
+        System.out.println("Soma");
+        System.out.println("-------------------------------------");
+        System.out.println("Primeiro numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Segundo numero: ");
+        numero2 = sc.nextDouble();
+        Calculadora.soma(numero1,numero2);
+        System.out.println("=====================================");
+        System.out.println("Subtracao");
+        System.out.println("-------------------------------------");
+        System.out.println("Primeiro numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Segundo numero: ");
+        numero2 = sc.nextDouble();
+        Calculadora.subtracao(numero1,numero2);
+        System.out.println("=====================================");
+        System.out.println("Multiplicacao");
+        System.out.println("-------------------------------------");
+        System.out.println("Primeiro numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Segundo numero: ");
+        numero2 = sc.nextDouble();
+        Calculadora.multiplicacao(numero1,numero2);
+        System.out.println("=====================================");
+        System.out.println("Divisao");
+        System.out.println("-------------------------------------");
+        System.out.println("Primeiro numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Segundo numero: ");
+        numero2 = sc.nextDouble();
+        Calculadora.divisao(numero1,numero2);
+
     }
 }
